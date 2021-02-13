a79de27308e43c7315c8ad45c75c61825a66c32b
schamblee-auth0 <stephanie.chamblee@auth0.com>
2021-02-13 07:38:43 -0500

    Add auth

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
 const express = require('express');
 const storage = require('node-persist');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', async (req, res) => {
+app.use((error, req, res, next) => {
+  return res.status(500).json({ error: error.toString() });
+});
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the [Key ID](https://tools.ietf.org/html/rfc7515#section-4.1.4) header parameter ("kid") and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+app.get('/', checkJwt, async (req, res) => {
   try {
     await storage.init();
     const userCount = await storage.getItem('userCount');
     res.send({userCount: userCount});
-  } catch (e) {
-    console.error(e)
+  } catch (error) {
+    next(error);
   }
 });
 
-app.post('/', async (req, res) => {
+app.post('/', checkJwt, async (req, res) => {
   try {
     await storage.init();
-    let oldUserCount = await storage.getItem('userCount')
+    let oldUserCount = await storage.getItem('userCount');
     oldUserCount++;
-    const newUserCount = await storage.setItem('userCount', oldUserCount)
+    const newUserCount = await storage.setItem('userCount', oldUserCount);
     const userCount = newUserCount.content.value;
     res.send({userCount});
-  } catch (e) {
-    console.error(e)
+  } catch (error) {
+    next(error);
   }
 });
 
